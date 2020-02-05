0d44c6356543caf93f0d852201d0000a431432a8
Austin SALVATORE <austin.leonardo.salvatore@gmail.com>
2020-02-05 23:41:08 +0100

    Creation: Project Framework Build

---
+++ ecommerce-server-end/src/main/java/com/austin/it/ecommerceserverend/EcommerceServerEndApplication.java
@@ -0,0 +1,13 @@
+package com.austin.it.ecommerceserverend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceServerEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceServerEndApplication.class, args);
+	}
+
+}
